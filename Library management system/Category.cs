1b7178d79571ae123032af8feda597ef23c24ce1
Elen <elen.v.grigoryan@gmail.com>
2024-02-01 01:05:42 +0400

    Add project files.

---
+++ Library management system/Category.cs
@@ -0,0 +1,14 @@
+﻿// Category.cs
+using System;
+
+sealed class Category
+{
+    public string CategoryName { get; private set; }
+    public string Description { get; private set; }
+
+    public Category(string categoryName, string description)
+    {
+        CategoryName = categoryName;
+        Description = description;
+    }
+}
